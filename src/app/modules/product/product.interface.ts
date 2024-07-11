b761004fb77b270462eab5a2dccf897619335b29
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-07-11 16:00:17 +0600

    Created product interface

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,6 +1,10 @@
 export type TProduct = {
+  _id: string;
+  image: string;
   title: string;
+  category: string;
   description: string;
   price: number;
   rating: number;
+  quantity: number;
 };
