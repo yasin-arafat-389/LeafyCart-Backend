1414d172e7445de1b1a3c861c5a923e661803b89
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-07-11 17:45:31 +0600

    Resolved cors issue

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,9 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(
+  cors({ origin: ['http://localhost:5173', 'https://leafycart-389.web.app'] }),
+);
 
 // application routes
 app.use('/api/v1/products', ProductRoutes);
